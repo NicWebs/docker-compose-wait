93615c2d9db28d30ac853f30805f5dd0065e8f56
NicWebs <wwebster@decisiohealth.com>
2020-10-01 15:13:49 -0500

    Linting

--- docker_compose_wait.py
+++ docker_compose_wait.py
@@ -5,16 +5,18 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 import subprocess
 import re
 import time
-import sys
 import argparse
 import yaml
 from timeparse import timeparse
 
+
 def call(args):
     return '\n'.join(subprocess.check_output(args).decode().splitlines())
 
+
 def get_all_statuses():
     return [tuple(x.split(",")) for x in call(["docker", "ps", "--all", "--format", "{{.ID}},{{.Status}}"]).splitlines()]
+
 
 def get_statuses_for_ids(ids):
     status_list = get_all_statuses()
@@ -29,6 +31,7 @@ def get_statuses_for_ids(ids):
             status = "removed"
         statuses[id] = status
     return statuses
+
 
 def convert_status(s):
     res = re.search(r"^([^\s]+)[^\(]*(?:\((.*)\).*)?$", s)
@@ -48,8 +51,10 @@ def convert_status(s):
     else:
         return "down"
 
+
 def get_converted_statuses(ids):
     return dict([(k, convert_status(v)) for k, v in get_statuses_for_ids(ids).items()])
+
 
 def get_docker_compose_args(args):
     nargs = []
@@ -58,6 +63,7 @@ def get_docker_compose_args(args):
     if args.project_name:
         nargs += ['-p', args.project_name]
     return nargs
+
 
 def get_services_ids(dc_args):
     services_names = yaml.safe_load(call(["docker-compose"] + dc_args + ["config"]))["services"].keys()
@@ -69,9 +75,11 @@ def get_services_ids(dc_args):
         services[name] = id
     return services
 
+
 def get_services_statuses(services_with_ids, exception_list):
     statuses_by_id = get_converted_statuses(services_with_ids.values())
     return dict([(k, statuses_by_id[v]) for k, v in services_with_ids.items() if k not in exception_list])
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -79,16 +87,16 @@ def main():
         usage='docker-compose-wait.py [options]'
         )
     parser.add_argument('-f', '--file', action='append', default=[],
-                    help='Specify an alternate compose file (default: docker-compose.yml)')
+                        help='Specify an alternate compose file (default: docker-compose.yml)')
     parser.add_argument('-p', '--project-name',
-                    help='Specify an alternate project name (default: directory name)')
+                        help='Specify an alternate project name (default: directory name)')
     parser.add_argument('-w', '--wait', action='store_true',
-                    help='Wait for all the processes to stabilize before exit (default behavior is to exit '
-                    + 'as soon as any of the processes is unhealthy)')
+                        help='Wait for all the processes to stabilize before exit (default behavior is to exit '
+                        + 'as soon as any of the processes is unhealthy)')
     parser.add_argument('-t', '--timeout', default=None,
-                    help='Max amount of time during which this command will run (expressed using the '
-                    + 'same format than in docker-compose.yml files, example: 5s, 10m,... ). If there is a '
-                    + 'timeout this command will exit returning 1. (default: wait for an infinite amount of time)')
+                        help='Max amount of time during which this command will run (expressed using the '
+                        + 'same format than in docker-compose.yml files, example: 5s, 10m,... ). If there is a '
+                        + 'timeout this command will exit returning 1. (default: wait for an infinite amount of time)')
     parser.add_argument('-b', '--blacklist', type=str, default=None,
                         help='Comma-delimited list of services to exclude from wait. i.e. services that could be'
                         + 'unhealthy.'
@@ -131,6 +139,7 @@ def main():
             exit(1)
 
         time.sleep(1)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
